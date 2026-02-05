aae4b00051e7659ed1d1c8f403c1cb8737c8701a
Reyklara <newprogrammer1984@gmail.com>
2026-02-05 20:49:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let contador =1
+document.querySelector("#bola1").checked = true
+
+setInterval( function(){
+    proxima();
+},5000)
+
+function proxima(){
+    contador = contador + 1
+    if(contador > 3){
+        contador = 1
+    }
+    document.querySelector("#bola"+contador).checked = true
+}
+
